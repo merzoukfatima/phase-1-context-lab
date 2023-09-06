d47d2e52d8b8249e0d22a8a9cd8820d357ba4d42
MerzoukFatimaZohra <merzoukfatimazahraa@gmail.com>
2023-09-07 00:22:35 +0100

    Assignment Completed

--- index.js
+++ index.js
@@ -8,7 +8,56 @@
  As a result, the lessons for this function will pass *and* it will be available
  for you to use if you need it!
  */
+// Your code here
+const createEmployeeRecord = ([firstName, familyName, title, payPerHour]) => {
+    return {
+      firstName: firstName,
+      familyName: familyName,
+      title: title,
+      payPerHour: payPerHour,
+      timeInEvents: [],
+      timeOutEvents: [],
+    };
+  };
+  const createEmployeeRecords = (array) => {
+    return array.map((item) => createEmployeeRecord(item));
+  };
+  
+const createTimeInEvent = function (dateStamp) {
+    const [date, hour] = dateStamp.split(" ");
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(hour, 10),
+        date: date,
+    });
+    return this;
+};
+  const createTimeOutEvent = function(dateStamp){
+    const [date, hour] = dateStamp.split(" ");
+    this.timeOutEvents.push({
+      type: "TimeOut",
+      hour: parseInt(hour, 10),
+      date: date,
+    });
+    return this;
+  };
+  const hoursWorkedOnDate = function(dateStamp){
+    const timeInEvents = this.timeInEvents.find(
+      (item) => item.date === dateStamp
+    );
+    const timeOutEvents = this.timeOutEvents.find(
+      (item) => item.date === dateStamp
+    );
+    return (timeOutEvents.hour - timeInEvents.hour) / 100;
+  };
+  const wagesEarnedOnDate = function(dateStamp){
+    return (
+      this.payPerHour * hoursWorkedOnDate.call(this,dateStamp)
+    );
+  };
 
+
+  
 const allWagesFor = function () {
     const eligibleDates = this.timeInEvents.map(function (e) {
         return e.date
@@ -21,3 +70,10 @@ const allWagesFor = function () {
     return payable
 }
 
+const findEmployeeByFirstName = (srcArray, firstName) => {
+  return srcArray.find((item) => item.firstName===firstName)
+}
+  const calculatePayroll = (employeeRecords) => {
+    return employeeRecords.reduce((total, item) => total + allWagesFor.call(item), 0);
+  };
+ 
